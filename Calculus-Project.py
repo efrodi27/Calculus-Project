cd3dbb522e45592e72acbe6d3c849794cd451d2b
efrodi27 <44811945+efrodi27@users.noreply.github.com>
2018-11-08 10:36:21 -0500

    Updated from Brython Server: 11/8/2018 10:36:22 AM

--- Calculus-Project.py
+++ Calculus-Project.py
@@ -1,1 +1,18 @@
-Hi
+#Eliza and Joey
+#Calc Project: Chapter 5
+
+import math
+
+print('FUNCTION TYPE')
+A = float(input('If function is polynomial, enter 1: '))
+
+if A==1:
+    a1 = float(input('Enter number of terms in function: '))
+    if a1==1:
+        b1 = float(input('Enter coefficient of a: '))
+        b2 = float(input('Enter coefficient of b: '))
+        b3 = float(input('Enter coefficient of a: '))
+
+print('CLOSED INTERVAL')
+b = float(input('Smallest X-value in interval'))
+e = float(input('Largest X-value in interval'))
