2c7d1cd3dc631e5ec008262d0ab6e8d97dd59a10
efrodi27 <44811945+efrodi27@users.noreply.github.com>
2018-11-08 10:43:56 -0500

    Updated from Brython Server: 11/8/2018 10:43:58 AM

--- Calculus-Project.py
+++ Calculus-Project.py
@@ -4,17 +4,20 @@
 import math
 
 print('FUNCTION TYPE')
-A = float(input('If function is polynomial, enter 1: '))
+A = float(input('Please enter function type: (polynomial = 1), (trigonometric = 2), (exponential = 3), and (log = 4): '))
+
+print('CLOSED INTERVAL')
+b = float(input('Smallest X-value in interval'))
+e = float(input('Largest X-value in interval'))
 
 if A==1:
     a1 = float(input('Enter number of terms in function: '))
     if a1==1:
         b1 = float(input('Enter leading coefficient: '))
+        D1 = b1
     elif a1==2:  
-        b1 = float(input('Enter coefficient of b: '))
-        b2 = float(input('Enter coefficient of a: '))
+        b1 = float(input('Enter leading coefficient: '))
+        b2 = float(input('Enter second coefficient: '))
     elif a1==3:
-
-print('CLOSED INTERVAL')
-b = float(input('Smallest X-value in interval'))
-e = float(input('Largest X-value in interval'))
+        b1 = float(input('Enter leading coefficient: '))
+        b2 = float(input('Enter second coefficient: '))
